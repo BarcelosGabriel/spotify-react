70755c233ce99bfaae8c2955213fcf32132a2735
BarcelosGabriel <gabriel.barcelos.araujo@outlook.com>
2025-02-02 11:45:52 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from '../src/components/Header/Header';
+import Footer from '../src/components/Footer/Footer';
+import Main from '../src/components/Main/Main';
+import Sidebar from '../src/components/Sidebar/Sidebar';
+
+function App() {
+  return (
+    <div>
+      <Sidebar/>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
